80c92817875fbbe97b092179312f6d41e2eae46f
Akash Mane <akashmane277@gmail.com>
2024-11-17 16:57:46 +0530

    api url update

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: `http://localhost:5000/api`,
+  // baseURL: `http://localhost:5000/api`,
+  baseURL: `https://health-portal-api.onrender.com`,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
